c3c6d5d67f40937d8ad50e8a0382eddae344bbe6
Maksym Starkov <mstarkov@panamint.kcir.pwr.edu.pl>
2020-03-16 15:51:15 +0100

    dodzalaz

---
+++ src/WyrazenieZesp.cpp
@@ -0,0 +1,12 @@
+#include "WyrazenieZesp.hh"
+
+
+/*
+ * Tu nalezy zdefiniowac funkcje, ktorych zapowiedzi znajduja sie
+ * w pliku naglowkowym.
+ */
+void Wyswietl(WyrazenieZesp  WyrZ)
+{
+  
+
+LZespolona Oblicz(WyrazenieZesp  WyrZ);
