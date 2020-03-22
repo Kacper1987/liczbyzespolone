5d7ceae801f3596578a1e5d1437576c54856449d
StarkovMaksym <62206059+StarkovMaksym@users.noreply.github.com>
2020-03-22 15:13:20 +0100

    Update LZespolona.hh

--- inc/LZespolona.hh
+++ inc/LZespolona.hh
@@ -1,28 +1,40 @@
 #ifndef LZESPOLONA_HH
 #define LZESPOLONA_HH
 
+#include <iostream>
+#include <cmath>
+
 /*!
  *  Plik zawiera definicje struktury LZesplona oraz zapowiedzi
- *  przeciazen operatorow arytmetycznych dzialajacych na tej 
+ *  przeciazen operatorow arytmetycznych dzialajacych na tej
  *  strukturze.
  */
-
 
 /*!
  * Modeluje pojecie liczby zespolonej
  */
+
 struct  LZespolona {
   double   re;    /*! Pole repezentuje czesc rzeczywista. */
   double   im;    /*! Pole repezentuje czesc urojona. */
 };
 
-
 /*
  * Dalej powinny pojawic sie zapowiedzi definicji przeciazen operatorow
  */
 
-void Wyswietl(LZespolona  LZ);
 LZespolona  operator + (LZespolona  Skl1,  LZespolona  Skl2);
-
+LZespolona  operator - (LZespolona  Skl1,  LZespolona  Skl2);
+LZespolona  operator * (LZespolona  Skl1,  LZespolona  Skl2);
+LZespolona  operator / (LZespolona  Skl1,  LZespolona  Skl2);
+LZespolona operator / (LZespolona Skl1, float Skl2);
+LZespolona sprzezenie(LZespolona Skl1);
+float modul(LZespolona Skl1);
+bool operator == (LZespolona Skl1, LZespolona Skl2);
+bool operator != (LZespolona Skl1, LZespolona Skl2);
+bool WczytajL (LZespolona &L);
+bool WyswietlL (LZespolona  &L);
+LZespolona UtworzL (float Re, float Im);
 
 #endif
+
